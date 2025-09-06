b739e340d5541e86957922e5c5379588822eb2e8
mazen <mazen.ali.rushdy@gmail.com>
2025-09-06 10:47:19 +0300

    Data Types Assignment 2 => implementation: Add methods to print data types of integer 100

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
-This Is Assignment
-echo 15.2 + 14.7 + (10.5 + 10.5); // 50
-echo 15.2 + 14.7 + (10.5 + 10.5); // Integer
-*/
-
-// This Is Solution
-
-echo (int) (15.2 + 14.7 + (10.5 + 10.5));
+echo gettype(100);
 echo "<br>";
-echo gettype((int) (15.2 + 14.7 + (10.5 + 10.5))); // Integer
+var_dump(100);
