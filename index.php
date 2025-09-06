db5b5053dff58f05e22a0fc76c74c3bc51a501ed
mazen <mazen.ali.rushdy@gmail.com>
2025-09-06 11:46:07 +0300

    Variables And Constants Assignment 4 => implementation: Print predefined variables for Document Root, Server Name and System Root

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php 
 
-$a = 200;
-$b = &$a;
-$a = 100;
+echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "<br>";
 
-echo $b; // 100
+echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<br>";
+
+echo "System Root: " . $_SERVER['SystemRoot'] . "<br>";
+
+
+// "C:/xampp/htdocs"
+// "localhost"
+// "C:\WINDOWS"
