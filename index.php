8d3242063246bf3fce84df68f55dd6134c13addb
mazen <mazen.ali.rushdy@gmail.com>
2025-09-06 11:36:48 +0300

    Variables And Constants Assignment 2 => implementation: Print Web using five different echo methods

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php 
-$var = "Elzero Courses";
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Elzero Courses">
-    <title>Welcome To <?= $var ?></title>
-  </head>
-  <body>
-    <h1><?= $var ?></h1>
-    <p>Here In <?= $var ?> We Provide Front-End And Back-End Courses</p>
-    <hr>
-    <div><?= $var ?> Is The What You Need.</div>
-    <footer>All Right Reserved To <?= $var ?></footer>
-  </body>
-</html>
+
+$name = "elzero";
+$$name = "Web";
+
+echo $$name;
+echo "<br>";
+echo $elzero;
+echo "<br>";
+echo "${$name}";
+echo "<br>";
+echo "{$$name}";
+echo "<br>";
+echo "Web";
