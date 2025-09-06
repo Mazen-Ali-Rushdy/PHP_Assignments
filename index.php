88546ae2e5e24f07d52901d24e8fd09be9f586ad
mazen <mazen.ali.rushdy@gmail.com>
2025-09-06 11:22:20 +0300

    Data Types Assignment 7 => implementation: Add nested array structure for programming languages.

--- index.php
+++ index.php
@@ -1,9 +1,70 @@
 <?php
 
-echo (bool)"Hello PHP";
-echo '<br>';
-echo gettype((int)"Hello PHP");
+$arr = [
+    "FrontEnd" => [
+        "HTML",
+        "CSS",
+        "JS" => [
+            "Vuejs" => [
+                2 => "v2",
+                3 => "v3"
+            ],
+            "Reactjs",
+            "Svelte"
+            ],
 
-// Needed Output
-// 1
-// integer
+        "BackEnd" => [
+            "PHP", 
+            "MySQL",
+            "Security"
+        ],
+
+        "Git",
+        "GitHub",
+        "Testing" => [
+            "Unit Testing",
+            "End To End",
+            "Integration"
+        ]
+    ]
+];
+
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+
+// Array
+// (
+//   [FrontEnd] => Array
+//     (
+//       [0] => HTML
+//       [1] => CSS
+//       [JS] => Array
+//         (
+//           [Vuejs] => Array
+//             (
+//               [2] => v2
+//               [3] => v3
+//             )
+
+//           [0] => Reactjs
+//           [1] => Svelte
+//         )
+//     )
+
+//   [BackEnd] => Array
+//     (
+//       [0] => PHP
+//       [1] => MySQL
+//       [2] => Security
+//     )
+
+//   [0] => Git
+//   [1] => Github
+//   [Testing] => Array
+//     (
+//       [0] => Unit Testing
+//       [1] => End To End
+//       [2] => Integration
+//     )
+// )
