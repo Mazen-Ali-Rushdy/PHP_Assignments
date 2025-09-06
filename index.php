a8994f17ed396578a1f2b7edc5d19e68d0f61218
mazen <mazen.ali.rushdy@gmail.com>
2025-09-06 10:39:49 +0300

    Data Types Assignment 1

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-//echo 'Prevent Me From Running Please';
-#echo 'Prevent Me From Running Please';
-/*echo 'Prevent Me From Running Please';*/
+/*
+This Is Assignment
+echo 15.2 + 14.7 + (10.5 + 10.5); // 50
+echo 15.2 + 14.7 + (10.5 + 10.5); // Integer
+*/
+
+// This Is Solution
+
+echo (int) (15.2 + 14.7 + (10.5 + 10.5));
+echo "<br>";
+echo gettype((int) (15.2 + 14.7 + (10.5 + 10.5))); // Integer
