ab3385d7bea6bfb852e25aafcae7ede6bdee16fa
mazen <mazen.ali.rushdy@gmail.com>
2025-09-07 20:17:00 +0300

    Loop Assignment 2 =>  A Loop to print numbers with leading zeros for numbers less than 10

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+// Loop Assignments
+
+// Assignment 1
 $index = 10;
 
 for ($i = 10; $i >= 1; $i--) {
   echo "$i<br>";
 }
+echo "@@@@@@@@@@@<br>";
 
 // Needed Output
 // 10
@@ -17,3 +21,25 @@ for ($i = 10; $i >= 1; $i--) {
 // 3
 // 2
 // 1
+
+// Assignment 2
+$start = 10;
+$end = 0;
+$stop = 3;
+
+for ($i = $start; $i >= $stop; $i--) {
+  if ($i < 10) {
+    echo "0";
+  }
+  echo $i . "<br>";
+}
+
+// Needed Output
+// 10
+// 09
+// 08
+// 07
+// 06
+// 05
+// 04
+// 03
