9245ef98725e77230cabb8499972ac1c3a0f1fff
mazen <mazen.ali.rushdy@gmail.com>
2025-09-07 20:29:00 +0300

    Loop Assignment 4 =>  Iterate through money array to display names and amounts

--- index.php
+++ index.php
@@ -59,3 +59,17 @@ echo "@@@@@@@@@@@<br>";
 // 2
 // 3
 // 4
+
+// Assignment 4
+$money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
+
+foreach($money as $name => $cost) {
+  echo "The Name Is $name And I Need $cost Pound From Him<br>";
+}
+echo "@@@@@@@@@@@<br>";
+
+// Output
+// "The Name Is Ahmed And I Need 100 Pound From Him"
+// "The Name Is Sayed And I Need 150 Pound From Him"
+// "The Name Is Osama And I Need 100 Pound From Him"
+// "The Name Is Maher And I Need 250 Pound From Him"
