04d5df5db98d85bba5fa8024bd3c047e791d6a52
mazen <mazen.ali.rushdy@gmail.com>
2025-09-07 21:35:22 +0300

    Function Assignment 2 => Function to collect and return variable number of arguments

--- index.php
+++ index.php
@@ -5,16 +5,31 @@
 // Assignment 1
 // Write Function Content Here
 function greeting($name, $gender = null) {
-    $title = "";
-    if ($gender === "Male") {
-        $title = "Mr ";
-    } elseif ($gender === "Female") {
-        $title = "Miss ";
-    }
-    return "Hello " . $title . $name . "<br>";
+  $title = "";
+  if ($gender === "Male") {
+    $title = "Mr ";
+  } elseif ($gender === "Female") {
+    $title = "Miss ";
+  }
+  return "Hello " . $title . $name . "<br>";
 }
 
 // Needed Output
 echo greeting("Osama", "Male"); // Hello Mr Osama
 echo greeting("Eman", "Female"); // Hello Miss Eman
 echo greeting("Sameh"); // Hello Sameh
+
+// Assignment 2
+// Write Function Content Here
+
+function get_arguments(...$args) {
+  $result = "";
+  foreach ($args as $arg) {
+    $result .= $arg . " ";
+  }
+  return $result . "<br>";
+} 
+
+// Needed Output
+echo get_arguments("Hello", "Elzero", "Web", "School"); // Hello Elzero Web School
+echo get_arguments("I", "Love", "PHP"); // I Love PHP
