831781745f25cb7e9b83bea1604b5a467693e839
mazen <mazen.ali.rushdy@gmail.com>
2025-09-07 21:28:26 +0300

    Function Assignment 1 => Create a greeting function with optional gender-based titles

--- index.php
+++ index.php
@@ -1,111 +1,20 @@
 <?php
 
-// Loop Assignments
+// Function Assignments
 
 // Assignment 1
-$index = 10;
-
-for ($i = 10; $i >= 1; $i--) {
-  echo "$i<br>";
+// Write Function Content Here
+function greeting($name, $gender = null) {
+    $title = "";
+    if ($gender === "Male") {
+        $title = "Mr ";
+    } elseif ($gender === "Female") {
+        $title = "Miss ";
+    }
+    return "Hello " . $title . $name . "<br>";
 }
-echo "@@@@@@@@@@@<br>";
 
 // Needed Output
-// 10
-// 9
-// 8
-// 7
-// 6
-// 5
-// 4
-// 3
-// 2
-// 1
-
-// Assignment 2
-$start = 10;
-$end = 0;
-$stop = 3;
-
-for ($i = $start; $i >= $stop; $i--) {
-  if ($i < 10) {
-    echo "0";
-  }
-  echo $i . "<br>";
-}
-echo "@@@@@@@@@@@<br>";
-
-// Needed Output
-// 10
-// 09
-// 08
-// 07
-// 06
-// 05
-// 04
-// 03
-
-// Assignment 3
-$start = 0;
-$mix = [1, 2, 3, "A", "B", "C", 4];
-
-for ($i = $start; $i < count($mix); $i++) {
-  if ($mix[$i] == $mix[$start] || is_string($mix[$i])) continue;
-  echo $mix[$i] . "<br>";
-}
-echo "@@@@@@@@@@@<br>";
-
-// Output
-// 2
-// 3
-// 4
-
-// Assignment 4
-$money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
-
-foreach($money as $name => $cost) {
-  echo "The Name Is $name And I Need $cost Pound From Him<br>";
-}
-echo "@@@@@@@@@@@<br>";
-
-// Output
-// "The Name Is Ahmed And I Need 100 Pound From Him"
-// "The Name Is Sayed And I Need 150 Pound From Him"
-// "The Name Is Osama And I Need 100 Pound From Him"
-// "The Name Is Maher And I Need 250 Pound From Him"
-
-// Assignment 4
-$nums = [1, 13, 12, 20, 51, 17, 30];
-
-$i = 0;
-
-while ($i < count($nums)) {
-  if ($nums[$i] % 2 == 0) {
-    echo $nums[$i] / 2;
-    echo "<br>";
-  }
-  $i++;
-}
-echo "@@@@@@@@@@@<br>";
-
-// Output
-// 6
-// 10
-// 15
-
-// Assignment 5
-$help_num = 4;
-$nums = [2, 4, 5, 6, 10];
-
-foreach($nums as $num) {
-  echo "$num + {$nums[$help_num]} = " . $num + $nums[$help_num] . "<br>";
-  $help_num--;
-}
-
-// Output
-// "2 + 10 = 12"
-// "4 + 6 = 10"
-// "5 + 5 = 10"
-// "6 + 4 = 10"
-// "10 + 2 = 12"
-echo "@@@@@@@@@@@<br>";
+echo greeting("Osama", "Male"); // Hello Mr Osama
+echo greeting("Eman", "Female"); // Hello Miss Eman
+echo greeting("Sameh"); // Hello Sameh
