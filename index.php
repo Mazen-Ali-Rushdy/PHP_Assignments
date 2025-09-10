7bb987160d1c9aa27e1f68aaa959bf221e3cbae9
mazen <mazen.ali.rushdy@gmail.com>
2025-09-10 21:50:20 +0300

    Math Functions And Filters Assignment 4 => Combine filter names with their IDs using filter_list and filter_id

--- index.php
+++ index.php
@@ -28,3 +28,41 @@ $num3 = -7.5; // -7
 echo round($num1, 0, PHP_ROUND_HALF_DOWN) . "<br>";
 echo round($num2, 1) . "<br>";
 echo round($num3, 0, PHP_ROUND_HALF_ODD) . "<br>";
+
+// Assignment 3
+
+$values = filter_list();
+$kes = [];
+
+foreach ($values as $value) {
+    array_push($kes, filter_id($value));
+}
+
+$filters = array_combine($kes, $values);
+
+echo "<pre>";
+print_r($filters);
+echo "</pre>";
+// Array
+// (
+//   [257] => int
+//   [258] => boolean
+//   [259] => float
+//   [272] => validate_regexp
+//   [277] => validate_domain
+//   [273] => validate_url
+//   [274] => validate_email
+//   [275] => validate_ip
+//   [276] => validate_mac
+//   [513] => stripped
+//   [514] => encoded
+//   [515] => special_chars
+//   [522] => full_special_chars
+//   [516] => unsafe_raw
+//   [517] => email
+//   [518] => url
+//   [519] => number_int
+//   [520] => number_float
+//   [523] => add_slashes
+//   [1024] => callback
+// )
