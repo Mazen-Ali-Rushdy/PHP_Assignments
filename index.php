22cd466a8c4b0346f451b8461df38c8d869dcf4e
mazen <mazen.ali.rushdy@gmail.com>
2025-09-10 21:40:25 +0300

    Math Functions And Filters Assignment 3 => Use round() with different modes for number rounding

--- index.php
+++ index.php
@@ -14,8 +14,17 @@ echo rand(11, 19);
 // Assignment 2
 $friends = ["Osama", "Ahmed", "Sayed", "Ibrahim"];
 
-echo "<Br>" . $friends[array_rand($friends)];
+echo "<Br>" . $friends[array_rand($friends)] . "<br>";
 
 // Example
 // Ibrahim
 // Ahmed
+
+// Assignment 3
+$num1 = 11.5; // 11
+$num2 = 9.4898; // 9.5
+$num3 = -7.5; // -7
+
+echo round($num1, 0, PHP_ROUND_HALF_DOWN) . "<br>";
+echo round($num2, 1) . "<br>";
+echo round($num3, 0, PHP_ROUND_HALF_ODD) . "<br>";
