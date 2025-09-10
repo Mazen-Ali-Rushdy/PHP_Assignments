4c590dfa9143ae71b5983fa161b9f2c9375f37bc
mazen <mazen.ali.rushdy@gmail.com>
2025-09-10 21:35:17 +0300

    Math Functions And Filters Assignment 2 => display a random element from an array

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 // ======== Math Functions And Filters ========
 
+// Assignment 1
 echo rand(11, 19);
 
 // Example
@@ -10,3 +11,11 @@ echo rand(11, 19);
 // 19
 // 12
 
+// Assignment 2
+$friends = ["Osama", "Ahmed", "Sayed", "Ibrahim"];
+
+echo "<Br>" . $friends[array_rand($friends)];
+
+// Example
+// Ibrahim
+// Ahmed
