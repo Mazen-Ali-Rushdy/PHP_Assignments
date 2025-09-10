e94d7c2ee2a8ff01353c750211a00723671d048b
mazen <mazen.ali.rushdy@gmail.com>
2025-09-10 20:13:39 +0300

    Array Built-In Functions Assignment 1 => Group friends into chunks and convert keys to lowercase

--- index.php
+++ index.php
@@ -1,60 +1,39 @@
 <?php
 
-// ======== String Built-In Functions ========
+// ======== Array Built-In Functions ========
 
 // **** Assignment 1 ****
-$num_one = -1;
-$num_two = 2.5;
+$friends = [
+  "AG" => "Ahmed Gamal",
+  "OM" => "Osama Mohamed",
+  "MG" => "Mahmoud Gamal",
+  "AS" => "Ahmed Samy",
+  "FA" => "Farid Ahmed",
+  "SM" => "Sayed Mohamed"
+];
 
-$let_one = "o";
-$let_two = "z";
+$friends = array_chunk(array_change_key_case($friends), 2);
 
-$str = "El%er0";
-
-$str = str_replace([$str[(int)$num_two], $str[$num_one]], [$let_two, $let_one], $str);
-
-echo $str . "<br>"; // Elzero
-
-// **** Assignment 2 ****
-$str = "Orezle";
-
-echo ucfirst(strrev(strtolower($str))) . "<br>";
-
-// Elzero
-
-// **** Assignment 3 ****
-$str = 'aAa';
-$num = 3;
-$char = "_";
-
-echo str_repeat(strtolower($str) . $char, $num) . '<br>';
-
-// aaa_aaa_aaa_
-
-// **** Assignment 3 ****
-$str = "<div><b>Elzero</b></div>";
-
-echo strip_tags($str, "<b>") . "<br>";
-
-// <b>Elzero</b>
-
-// **** Assignment 3 ****
-$str = "Elzero Web School";
-$e = "e";
-$o = "O";
-$four = 4;
-
-echo substr_count($str, $e, $four) . "<br>";
-echo substr_count($str, $e) . "<br>";
-
-// 1
-// 2
-
-// **** Assignment 3 ****
-$chars = ["E", "l", "z", "e", "r", "o"];
-
-echo implode($chars) . "<br>";
+echo "<pre>";
+print_r($friends);
+echo "</pre>";
 
 // Output
-// "Elzero"
-
+// Array
+// (
+//   [0] => Array
+//     (
+//       [ag] => Ahmed Gamal
+//       [om] => Osama Mohamed
+//     )
+//   [1] => Array
+//     (
+//       [mg] => Mahmoud Gamal
+//       [as] => Ahmed Samy
+//     )
+//   [2] => Array
+//     (
+//       [fa] => Farid Ahmed
+//       [sm] => Sayed Mohamed
+//     )
+// )
