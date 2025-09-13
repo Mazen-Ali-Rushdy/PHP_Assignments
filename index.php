5ef689298931b975f8cf46364eac3ea23429af07
mazen <mazen.ali.rushdy@gmail.com>
2025-09-13 09:58:53 +0300

    File System Assignment 2 =>  Create and remove nested directories with specific permissions

--- index.php
+++ index.php
@@ -9,3 +9,18 @@ echo round(disk_total_space("C:") / 1024 / 1024 / 1024 / 1024, 2) . " Terabyte<b
 // Output Examples
 // "1.37 Terabyte"
 // "0.46 Terabyte"
+
+// Assignment 2
+if (!file_exists("Programming/PHP")) {
+    mkdir("Programming/PHP", 0700, true);
+}
+else {
+    rmdir("Programming/PHP");
+    echo "Directory Programming/PHP Removed<br>";
+    rmdir("Programming");
+    echo "Directory Programming Removed<br>";
+}
+
+// Output
+// "Directory Programming/PHP Removed"
+// "Directory Programming Removed"
