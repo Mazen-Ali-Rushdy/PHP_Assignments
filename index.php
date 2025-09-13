4fb7b63080fa40bbf5a279f49394b4baf27ed8eb
mazen <mazen.ali.rushdy@gmail.com>
2025-09-13 10:17:03 +0300

    File System Assignment 3 =>  Read and print the first two lines of a file using three different methods

--- index.php
+++ index.php
@@ -24,3 +24,25 @@ else {
 // Output
 // "Directory Programming/PHP Removed"
 // "Directory Programming Removed"
+
+// Assignment 3
+
+// elzero.txt Content
+// Hello Elzero Web
+// School
+// No Need To Read
+
+// first Way
+$handel = fopen("elzero.txt","r");
+echo fgets($handel) . fgets($handel) . "<br>";
+
+// Second Way
+echo file_get_contents("elzero.txt", false, null, 0, 23) . "<br>";
+
+// Third Way
+echo file("elzero.txt")[0] . file("elzero.txt")[1] . "<br>";
+
+// Needed Output
+// "Hello Elzero Web School"
+// "Hello Elzero Web School"
+// "Hello Elzero Web School"
