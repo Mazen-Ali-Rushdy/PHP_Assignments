4cedcc292b7a923a141205cbcd88297f89ca183f
mazen <mazen.ali.rushdy@gmail.com>
2025-09-13 11:10:33 +0300

    Sessions & Cookies Assignment 1 => Set cookies with a specific expiration time

--- index.php
+++ index.php
@@ -1,28 +1,9 @@
 <?php
 
-// ======== Date & Time ========
+// ======== Sessions & Cookies ========
 
 // Assignment 1
-date_default_timezone_set("Asia/Riyadh");
-echo date_default_timezone_get() . "<br>";
-echo date_format(date_create("now"), "D, d M y - h:i:s A") . "<br>";
-echo date_format(date_create("now"), "l, d F Y - h:i:s A") . "<br>";
 
-// Output Needed
-// "Asia/Riyadh"
-// "Wed, 09 Nov 22 - 07:11:22 PM"
-// "Wednesday, 09 November 2022 - 07:11:22 PM"
-
-// Assignment 2
-date_default_timezone_set("Africa/Cairo");
-echo date_default_timezone_get() . "<br>";
-
-$date = "2005-10-02";
-
-$date = date_create( "2005-10-02");
-date_add($date, date_interval_create_from_date_string("15 hours 15 minutes 15 seconds"));
-echo date_format($date, "Y, F, l H:i:s");
-
-// Output Needed
-// "Africa/Cairo"
-// "2005, October, Sunday '02nd' 15:15:15"
+setcookie("site[layout]", "Boxed", strtotime("+2 months 5 days"), "/");
+setcookie("site[font]", "Swat", strtotime("+2 months 5 days"), "/");
+setcookie("site[color]", "Blue", strtotime("+2 months 5 days"), "/");
