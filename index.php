85543b33c0fd858d4f62a65c81cf6a5178b36d3d
mazen <mazen.ali.rushdy@gmail.com>
2025-09-13 11:18:24 +0300

    Sessions & Cookies Assignment 2 => Retrieve and display cookie values

--- index.php
+++ index.php
@@ -3,7 +3,26 @@
 // ======== Sessions & Cookies ========
 
 // Assignment 1
-
 setcookie("site[layout]", "Boxed", strtotime("+2 months 5 days"), "/");
 setcookie("site[font]", "Swat", strtotime("+2 months 5 days"), "/");
 setcookie("site[color]", "Blue", strtotime("+2 months 5 days"), "/");
+
+// Assignment 2
+$cookie_cont = $_COOKIE;
+
+echo "<pre>";
+print_r($cookie_cont);
+echo "</pre>";
+
+echo "Your Color Is " . $cookie_cont["site"]["color"] . " And Your Font Is " . $cookie_cont["site"]["font"] . "<br>";
+
+// Array
+// (
+//   [site] => Array
+//     (
+//       [color] => blue
+//       [font] => Swat
+//       [layout] => boxed
+//     )
+// )
+// "Your Color Is blue And Your Font Is Swat"
