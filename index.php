765f6ebf91515d5d1d2807d443fd4a080601b82d
mazen <mazen.ali.rushdy@gmail.com>
2025-09-13 10:25:12 +0300

    Date & Time Assignment 1 => Set Riyadh timezone and format dates in two different styles

--- index.php
+++ index.php
@@ -1,48 +1,14 @@
 <?php
 
-// ======== File System ========
+// ======== Date & Time ========
 
 // Assignment 1
-echo round(disk_total_space("E:") / 1024 / 1024 / 1024 / 1024, 2) . " Terabyte<br>";
-echo round(disk_total_space("C:") / 1024 / 1024 / 1024 / 1024, 2) . " Terabyte<br>";
+date_default_timezone_set("Asia/Riyadh");
+echo date_default_timezone_get() . "<br>";
+echo date_format(date_create("now"), "D, d M y - h:i:s A") . "<br>";
+echo date_format(date_create("now"), "l, d F Y - h:i:s A") . "<br>";
 
-// Output Examples
-// "1.37 Terabyte"
-// "0.46 Terabyte"
-
-// Assignment 2
-if (!file_exists("Programming/PHP")) {
-    mkdir("Programming/PHP", 0700, true);
-}
-else {
-    rmdir("Programming/PHP");
-    echo "Directory Programming/PHP Removed<br>";
-    rmdir("Programming");
-    echo "Directory Programming Removed<br>";
-}
-
-// Output
-// "Directory Programming/PHP Removed"
-// "Directory Programming Removed"
-
-// Assignment 3
-
-// elzero.txt Content
-// Hello Elzero Web
-// School
-// No Need To Read
-
-// first Way
-$handel = fopen("elzero.txt","r");
-echo fgets($handel) . fgets($handel) . "<br>";
-
-// Second Way
-echo file_get_contents("elzero.txt", false, null, 0, 23) . "<br>";
-
-// Third Way
-echo file("elzero.txt")[0] . file("elzero.txt")[1] . "<br>";
-
-// Needed Output
-// "Hello Elzero Web School"
-// "Hello Elzero Web School"
-// "Hello Elzero Web School"
+// Output Needed
+// "Asia/Riyadh"
+// "Wed, 09 Nov 22 - 07:11:22 PM"
+// "Wednesday, 09 November 2022 - 07:11:22 PM"
